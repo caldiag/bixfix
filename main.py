120865f052ebe8d25be57e5eb9bc544548661493
caldiag <caldiag@caldiag.com>
2024-11-05 13:08:59 -0300

    feat: add light mode argument

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ if __name__ == "__main__":
   parser = argparse.ArgumentParser(prog='bixfix', description='Verificar e corrigir erros comuns do agente Zabbix em servidores Rihappy.')
   parser.add_argument('user')
   parser.add_argument('password')
-  parser.add_argument('-p', '--passive',
+  parser.add_argument('-p', '--passive', # roda rotina de sondagem e gera logs, porém não efetua reinstalações ou ajustes.
                     action='store_true')
-  parser.add_argument('-f', '--force-fix')
+  parser.add_argument('-l', '--light-fix', # só atualiza arquivos de configuração
+                    action='store_true')
+  parser.add_argument('-f', '--force-fix') # deve ser parametrizada com um range de lojas (ex. 0,20), e força a reinstalação
   args = parser.parse_args()
 
   with open("stores.txt", 'r') as file:
     lines = file.readlines()
     stores = [line.strip() for line in lines]
 
-  probe.start(stores, args.user, args.password, args.passive, args.force_fix)
+  probe.start(stores, args)
