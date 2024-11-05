193b6863d526426cb62e075043641257d5a3f718
caldiag <caldiag@caldiag.com>
2024-11-05 13:09:58 -0300

    feat: remove logs if exists

--- utils/directory.py
+++ utils/directory.py
@@ -1,5 +1,8 @@
 import os
+import shutil
 
 def ensure_directory_exists(directory):
-    if not os.path.exists(f"../logs/{directory}"):
-        os.makedirs(f"../logs/{directory}")
+    if not os.path.exists(f"logs/{directory}"):
+        os.makedirs(f"logs/{directory}")
+        return
+    shutil.rmtree("logs")
